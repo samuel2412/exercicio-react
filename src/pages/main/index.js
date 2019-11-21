bf0db5f9750020a9a509dd4e90cd3b53179c9fea
Samuel Alves <samuel241296@hotmail.com>
2019-11-21 17:37:34 -0300

    css da parte do profile concluida

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,13 +3,13 @@ import api from '../../services/api';
 import './styles.css';
 
 
-export default class Main extends Component{
-    state={
-        profile: [ ],
-        contacts: [ ]
+export default class Main extends Component {
+    state = {
+        profile: [],
+        contacts: []
     };
 
-    componentDidMount(){
+    componentDidMount() {
         this.loadProfile();
     }
 
@@ -18,63 +18,68 @@ export default class Main extends Component{
 
         //console.log(response.data.profile);
 
-        const  { contact, ...profile} = response.data.profile;  //captura o profile
+        const { contact, ...profile } = response.data.profile;  //captura o profile
         //const { contacts } = response.data.profile.contact;
         //console.log(contacts);
 
-        
+
 
         this.setState({ contacts: contact, profile });   //salva no state
 
     }
 
 
-    render(){
+    render() {
         const { profile, contacts } = this.state;
         console.log(profile)
         console.log(contacts)
-       
 
 
-       // {this.state.contacts && this.state.contacts.map
-       return (
-       <div className="profile-page">
-           <div className="profile-profile">
-               <img src={ profile.image } alt="" ></img>
-                <h1> { profile.name}  </h1>
-                <p> { profile.profession } </p>
-                <p> { profile.description } </p>
-                <p> { contacts.tel } </p>
-                <p> { contacts.cel } </p>
-                <p> { contacts.address } </p>
-                <p> { contacts.website } </p>
-                <p> { contacts.mail } </p>
-                
-                {profile.skills && profile.skills.map(skill => (
-                    <article>
-                      <h3>{ skill.name }: { skill.value }</h3>               
 
-                    </article>
+        // {this.state.contacts && this.state.contacts.map
+        return (
+            <div className="profile-page">
+                <div className="profile-profile">
+                    <img src={profile.image} alt="" ></img>
 
-                ))}
-               
-           </div>
-           
+                    <h1 className="profile-name"> {profile.name}  </h1>
+                    <p className="profile-profession"> {profile.profession} </p>
+                    <h5 className="profile-subtitles">PROFILE</h5>
+                    <p> {profile.description} </p>
+                    <h5 className="profile-subtitles">CONTACT</h5>
+                    <p> {contacts.tel} <br></br>{contacts.cel} </p>
+                    <p> {contacts.address} </p>
+                    <p> {contacts.website} <br></br> {contacts.mail} </p>
+                    <h5 className="profile-subtitles">SKILLS</h5>
+                    {profile.skills && profile.skills.map(skill => (
+                        <article>
+                            <p className="profile-skill-name"> {skill.name}</p>
+                            <div style={{border: '1px solid #fff',backgroundColor:'#333'}}>
+                                <div style={{ height: '24px', width: `${skill.value}`,backgroundColor: 'white' }}></div>
+                            </div>
 
-           <div className="profile-experience">
-                {profile.experience && profile.experience.map(exp => (
-                    <article>
-                      <h3>{ exp.name }</h3>
-                      <p>{ exp.date }</p>
-                      <p>{ exp.description }</p>                 
+                            
 
-                    </article>
+                        </article>
+                    ))}
+                </div>
 
-                ))}
+
+                <div className="profile-experience">
+
+                    {profile.experience && profile.experience.map(exp => (
+                        <article>
+                            <h3>{exp.name}</h3>
+                            <p>{exp.date}</p>
+                            <p>{exp.description}</p>
+
+                        </article>
+
+                    ))}
+                </div>
+
+
             </div>
-
-           
-       </div> 
-       );
-        }
+        );
+    }
 }
