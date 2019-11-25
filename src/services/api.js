8bba012957652934a517f7b57aae2623b4dde975
Samuel Alves <samuel241296@hotmail.com>
2019-11-25 10:26:44 -0300

    outras correções de bugs no deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'www.mocky.io/v2/5a5e38f3330000b0261923a5'});
+const api = axios.create({baseURL: 'https://www.mocky.io/v2/5a5e38f3330000b0261923a5'});
 
 export default api;
